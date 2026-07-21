b33f9cef9e284805613ade934995da684897b132
Boris Pavlov <hedin@poznavshii.ru>
2026-07-21 13:44:02 +0300

    add minor changes
    change readme file
    change location detail scene

--- RickAndMorty/Scenes/LocationDetailScene/LocationDetailModule.swift
+++ RickAndMorty/Scenes/LocationDetailScene/LocationDetailModule.swift
@@ -5,4 +5,27 @@
 //  Created by Борис Павлов on 15.07.2026.
 //
 
-import Foundation
+import UIKit
+
+enum LocationDetailModule {
+    static func build(identifier: Int) -> UIViewController {
+        let view = LocationDetailViewController()
+        let presenter = LocationDetailPresenter()
+        let router = LocationDetailRouter()
+        let sectionManager = LocationDetailSectionManager()
+        let worker = LocationDetailWorker()
+        let interactor = LocationDetailInteractor(
+            presenter: presenter,
+            router: router,
+            worker: worker,
+            sectionManager: sectionManager,
+            identifier: identifier
+        )
+        
+        view.interactor = interactor
+        presenter.viewController = view
+        router.transitionHandler = view
+        
+        return view
+    }
+}
